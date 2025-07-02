fcd4d5240971c1e31bb323028ffa1b31571e07b6
Ashmit Avash <188277537+Kenshi0905@users.noreply.github.com>
2025-07-02 12:54:12 +0530

    Created the buttons and customised them

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,9 +6,15 @@ public class TicTacToe {
     int boardwidth = 600;
     int boardheight = 650; // fifty for text panel
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic Tac Toe");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
 
     TicTacToe() {
         frame.setVisible(true);
@@ -26,7 +32,25 @@ public class TicTacToe {
 
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLabel);
-        frame.add(textPanel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3, 3));
+        boardPanel.setBackground(Color.GRAY);
+        frame.add(boardPanel, BorderLayout.CENTER);
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                JButton tile = new JButton();
+                board[r][c] = tile;
+                boardPanel.add(tile);
+
+                tile.setBackground(Color.WHITE);
+                tile.setForeground(Color.BLACK);
+                tile.setFont(new Font("Verdana", Font.BOLD, 120));
+                
+            }
+
+        }
 
     }
 }
